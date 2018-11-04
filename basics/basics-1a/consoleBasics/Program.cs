dfbc668f23075bddc6f4ed4a48b320efa8e14054
Hafidz <halfpheez92@gmail.com>
2018-11-04 17:52:47 +0800

    added csharp training code.

---
+++ basics/basics-1a/consoleBasics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace consoleBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customString = "Custom String";
+            Console.WriteLine("Hardcoded string: Hello World!");
+            Console.WriteLine("Using var keyword: " + customString);
+        }
+    }
+}
