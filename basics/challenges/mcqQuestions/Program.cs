b7dbbef87c619b1f729dfd860100ad0f95c8b05b
Hafidz <halfpheez92@gmail.com>
2018-11-05 22:02:06 +0800

    modified code added conditions.

--- basics/challenges/mcqQuestions/Program.cs
+++ basics/challenges/mcqQuestions/Program.cs
@@ -18,6 +18,16 @@ namespace mcqQuestions
             Console.WriteLine("\nName: " + userName);
             Console.WriteLine("Age: " + userAge);
             Console.WriteLine("Month DOB: " + userMonthDOB);
+
+            if (userMonthDOB == "march" || userMonthDOB == "March") {
+              Console.WriteLine("Zodiac Sign: Aries");
+            }
+            else if (userMonthDOB == "april" || userMonthDOB == "April") {
+              Console.WriteLine("Zodiac Sign: Taurus");
+            }
+            else if (userMonthDOB == "may" || userMonthDOB == "May") {
+              Console.WriteLine("Zodiac Sign: Gemini");
+            }
         }
     }
 }
