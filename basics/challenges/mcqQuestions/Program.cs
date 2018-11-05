00e60429ae16d3b6a51efe1f21364d58e92bab87
Hafidz <halfpheez92@gmail.com>
2018-11-05 21:40:52 +0800

    solved challenge mcq implementation.

---
+++ basics/challenges/mcqQuestions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace mcqQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhats your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("\nHow old are you?");
+            var userAge =  Console.ReadLine();
+
+            Console.WriteLine("\nWhat month were your born in?");
+            var userMonthDOB = Console.ReadLine();
+
+            Console.WriteLine("\nName: " + userName);
+            Console.WriteLine("Age: " + userAge);
+            Console.WriteLine("Month DOB: " + userMonthDOB);
+        }
+    }
+}
