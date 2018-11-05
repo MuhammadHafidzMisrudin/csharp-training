469618c72d0dad11af7a1c96e86f9639732fdbf8
Hafidz <halfpheez92@gmail.com>
2018-11-05 23:48:27 +0800

    modified code, added for loop and foreach loop.

--- basics/basics-1a/fixedArrays/Program.cs
+++ basics/basics-1a/fixedArrays/Program.cs
@@ -8,10 +8,17 @@ namespace fixedArrays
         {
             var studentGrades = new int[] {75, 45, 56, 87, 90, 100, 43, 76, 89, 99};
 
-            // Console.WriteLine(studentGrades[5]);
-
+            // Using for loop.
+            Console.WriteLine("\nUsing for loop.");
             for (int i=0; i<studentGrades.Length; i++) {
               Console.WriteLine(studentGrades[i]);
+            }
+
+
+            // Using foreach loop.
+            Console.WriteLine("\nUsing foreach loop.");
+            foreach (var studentGrade in studentGrades) {
+              Console.WriteLine(studentGrade);
             }
         }
     }
