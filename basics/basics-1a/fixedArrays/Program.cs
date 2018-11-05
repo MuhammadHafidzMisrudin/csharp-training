860d0c6d7af8816efeda23aab145ee38c28bdfad
Hafidz <halfpheez92@gmail.com>
2018-11-05 23:41:06 +0800

    implemented Arrays.

---
+++ basics/basics-1a/fixedArrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace fixedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentGrades = new int[] {75, 45, 56, 87, 90, 100, 43, 76, 89, 99};
+
+            // Console.WriteLine(studentGrades[5]);
+
+            for (int i=0; i<studentGrades.Length; i++) {
+              Console.WriteLine(studentGrades[i]);
+            }
+        }
+    }
+}
