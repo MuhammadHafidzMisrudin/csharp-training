2f227d0843f19984360cd61df66ff10b47c576b2
Hafidz <halfpheez92@gmail.com>
2018-11-05 22:32:02 +0800

    solved passCode challenge with while loop.

---
+++ basics/challenges/passCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace passCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var code = "";
+          while (code != "secret") {
+
+            Console.WriteLine("What's the passcode?");
+            code = Console.ReadLine();
+
+            if (code != "secret") {
+              Console.WriteLine("Error! Not Authenticated.");
+            }
+          } // end while loop
+          Console.WriteLine("Authenticated.");
+        }
+    }
+}
