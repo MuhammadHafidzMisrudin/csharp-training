7b7bef8a614d3a21cfa2f3e99a4bea57aefb7185
Hafidz <halfpheez92@gmail.com>
2018-11-05 22:47:19 +0800

    implemented while loops and for loops.

---
+++ basics/basics-1a/forLoops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace forLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var num = 1;
+          while (num <= 10) {
+            Console.WriteLine(num);
+            num++;
+          }
+
+          for (int i=11; i<=20; i++) {
+            Console.WriteLine(i);
+          }
+        }
+    }
+}
