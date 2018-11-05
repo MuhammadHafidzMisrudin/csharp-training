ff7a92ea2d38336db7c24c1106f411007d97255a
Hafidz <halfpheez92@gmail.com>
2018-11-05 22:57:35 +0800

    solved loop counting challenge.

---
+++ basics/challenges/loopCounting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace loopCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          for (int a=1; a<=5; a++) {
+            for (int i=1; i<=10; i++) {
+              Console.WriteLine(i);
+            }
+            for (int j=10; j>=1; j--) {
+              Console.WriteLine(j);
+            }
+          }
+        }
+    }
+}
