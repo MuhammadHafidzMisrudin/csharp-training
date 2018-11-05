bb4d7e2732eb72ccd5ab7c267e01f38858c29186
Hafidz <halfpheez92@gmail.com>
2018-11-05 23:16:19 +0800

    uncommented loop.

--- basics/challenges/loopCounting/Program.cs
+++ basics/challenges/loopCounting/Program.cs
@@ -7,9 +7,9 @@ namespace loopCounting
         static void Main(string[] args)
         {
           for (int a=1; a<=5; a++) {
-            // for (int i=1; i<=10; i++) {
-            //   Console.WriteLine(i);
-            // }
+            for (int i=1; i<=10; i++) {
+              Console.WriteLine(i);
+            }
             for (int j=10; j>=1; j--) {
               Console.WriteLine(j);
             }
