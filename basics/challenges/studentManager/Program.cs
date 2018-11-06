eefc94ddbdc7c3c212866283d08816355bba7428
Hafidz <halfpheez92@gmail.com>
2018-11-07 00:47:20 +0800

    implemented solution for student manager challenge.

--- basics/challenges/studentManager/Program.cs
+++ basics/challenges/studentManager/Program.cs
@@ -6,7 +6,36 @@ namespace studentManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How many students?");
+            var numOfStudents = int.Parse(Console.ReadLine());
+            Console.WriteLine(numOfStudents);
+
+            var studentNamesArray = new string[numOfStudents];
+            // Console.WriteLine(studentNamesArray);
+
+            var studentGradesArray =  new int[numOfStudents];
+            // Console.WriteLine(studentGradesArray);
+
+            var studentName = "";
+            var studentGrade = 0;
+            for (int i=0; i<numOfStudents; i++) {
+              Console.WriteLine("Student Name: ");
+              studentName = Console.ReadLine();
+
+              studentNamesArray[i] = studentName;
+
+              Console.WriteLine("Student Grade: ");
+              studentGrade = int.Parse(Console.ReadLine());
+
+              studentGradesArray[i] = studentGrade;
+            }
+
+            Console.WriteLine("\n");
+            for (int j=0; j<numOfStudents; j++) {
+              Console.WriteLine("Name: " + studentNamesArray[j]);
+              Console.WriteLine("Grade: " + studentGradesArray[j]);
+              Console.WriteLine("\n");
+            }
         }
     }
 }
