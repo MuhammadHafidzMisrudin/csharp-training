413e1c3b5fbbde94bfc9d7cf3e023d51b4f06130
Hafidz <halfpheez92@gmail.com>
2018-11-07 00:19:11 +0800

    implemented switch statement.

--- basics/basics-1a/switchStatement/Program.cs
+++ basics/basics-1a/switchStatement/Program.cs
@@ -10,17 +10,35 @@ namespace switchStatement
 
             var years = int.Parse(Console.ReadLine());
 
-            if (years == 0) {
-              Console.WriteLine("Inexperienced");
-            } else if (years == 1) {
-              Console.WriteLine("Junior");
-            } else if (years == 2) {
-              Console.WriteLine("Intermediate");
-            } else if (years == 3) {
-              Console.WriteLine("Advanced");
-            } else  {
-              Console.WriteLine("Senior");
+            switch (years) {
+              case 0:
+                Console.WriteLine("Inexperienced");
+                break;
+              case 1:
+                Console.WriteLine("Junior");
+                break;
+              case 2:
+                Console.WriteLine("Intermediate");
+                break;
+              case 3:
+                Console.WriteLine("Advanced");
+                break;
+              default:
+                Console.WriteLine("Senior");
+                break;
             }
+
+            // if (years == 0) {
+            //   Console.WriteLine("Inexperienced");
+            // } else if (years == 1) {
+            //   Console.WriteLine("Junior");
+            // } else if (years == 2) {
+            //   Console.WriteLine("Intermediate");
+            // } else if (years == 3) {
+            //   Console.WriteLine("Advanced");
+            // } else  {
+            //   Console.WriteLine("Senior");
+            // }
         }
     }
 }
