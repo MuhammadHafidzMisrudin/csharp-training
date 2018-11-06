4c9c5c219a97203ba1adfbd8625724d857199ff0
Hafidz <halfpheez92@gmail.com>
2018-11-07 00:24:29 +0800

    added a project for student manager.

---
+++ basics/challenges/studentManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace studentManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
