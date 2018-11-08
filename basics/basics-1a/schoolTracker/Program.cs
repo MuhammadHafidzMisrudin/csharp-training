51e09859b1bcce3c4f5800436d261678c2c5c207
Hafidz <halfpheez92@gmail.com>
2018-11-08 14:45:44 +0800

    modified solution of school tracker with dynamic list collections.

--- basics/basics-1a/schoolTracker/Program.cs
+++ basics/basics-1a/schoolTracker/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace schoolTracker
 {
@@ -6,26 +7,62 @@ namespace schoolTracker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How many students in your class?");
-            var studentCount = int.Parse(Console.ReadLine());
+          // Uncomment this for test.
+          // Console.WriteLine("How many students in your class?");
+          // var studentCount = int.Parse(Console.ReadLine());
 
-            // Initialise and specify the sizes of Arrays.
-            var studentNames = new string[studentCount];
-            var studentGrades = new int[studentCount];
+          // Initialise and specify the sizes of Arrays.
+          // var studentNames = new string[studentCount];
+          // var studentGrades = new int[studentCount];
 
-            // Store and insert data for both Arrays.
-            for (int i=0; i<studentCount; i++) {
-              Console.WriteLine("Student Name: ");
-              studentNames[i] = Console.ReadLine();
+          // Initialise dynamic lists collection.
+          var studentNames = new List<string>();
+          var studentGrades = new List<int>();
 
-              Console.WriteLine("Student Grade: ");
-              studentGrades[i] = int.Parse(Console.ReadLine());
+          Console.WriteLine("Capacity studentNames: {0}", studentNames.Capacity);
+          Console.WriteLine("Capacity studentGrades: {0}", studentGrades.Capacity);
+
+          // Set a flag.
+          var addElement = true;
+
+          // Use while loop for dynamic data insertion.
+          while (addElement) {
+            Console.WriteLine();
+            Console.WriteLine("Student Name: ");
+            studentNames.Add(Console.ReadLine());
+
+            Console.WriteLine("Student Grade: ");
+            studentGrades.Add(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Add another? y/n: ");
+            var inputDetails = Console.ReadLine();
+
+            if (inputDetails != "y" && inputDetails != "Y") {
+              addElement = false;
             }
+          } // end while loop.
 
-            // Display output of student details.
-            for (int i=0; i<studentCount; i++) {
-              Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
-            }
+          Console.WriteLine();
+          Console.WriteLine("Capacity studentNames: {0}", studentNames.Capacity);
+          Console.WriteLine("Total studentNames: {0}", studentNames.Count);
+          Console.WriteLine("Capacity studentGrades: {0}", studentGrades.Capacity);
+          Console.WriteLine("Total studentNames: {0}", studentGrades.Count);
+          Console.WriteLine();
+
+          // Uncomment this for test.
+          // Store and insert data for both Arrays.
+          // for (int i=0; i<studentCount; i++) {
+          //   Console.WriteLine("Student Name: ");
+          //   studentNames[i] = Console.ReadLine();
+          //
+          //   Console.WriteLine("Student Grade: ");
+          //   studentGrades[i] = int.Parse(Console.ReadLine());
+          // }
+
+          // Display output of student details.
+          for (int i=0; i<studentNames.Count; i++) {
+            Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
+          }
         }
     }
 }
