f52190c65cdd2e77db293c70a24f4dee38fb8ddb
Hafidz <halfpheez92@gmail.com>
2018-11-11 00:49:36 +0800

    modified code implemented object oriented for school tracker.

--- basics/basics-1a/schoolTracker/Program.cs
+++ basics/basics-1a/schoolTracker/Program.cs
@@ -20,8 +20,10 @@ namespace schoolTracker
           var studentGrades = new List<int>();
           var studentList = new List<Student>();
 
-          Console.WriteLine("Capacity studentNames: {0}", studentNames.Capacity);
-          Console.WriteLine("Capacity studentGrades: {0}", studentGrades.Capacity);
+          // Console.WriteLine("Capacity studentNames: {0}", studentNames.Capacity);
+          // Console.WriteLine("Capacity studentGrades: {0}", studentGrades.Capacity);
+          Console.WriteLine("Capacity studentList: {0}", studentList.Capacity);
+
 
           // Set a flag.
           var addElement = true;
@@ -29,9 +31,12 @@ namespace schoolTracker
           // Use while loop for dynamic data insertion.
           while (addElement) {
 
+            // Create a student object.
             var studentObject = new Student();
 
             Console.WriteLine();
+
+            // Set propeties values.
             Console.WriteLine("Student Name: ");
             studentObject.Name = Console.ReadLine();
             // studentNames.Add(Console.ReadLine());
@@ -40,7 +45,16 @@ namespace schoolTracker
             studentObject.Grade = int.Parse(Console.ReadLine());
             // studentGrades.Add(int.Parse(Console.ReadLine()));
 
-            // Add new student details to the student list.
+            Console.WriteLine("Student Birthday: ");
+            studentObject.Birthday = Console.ReadLine();
+
+            Console.WriteLine("Student Address: ");
+            studentObject.Address = Console.ReadLine();
+
+            Console.WriteLine("Student Phone: ");
+            studentObject.Phone = int.Parse(Console.ReadLine());
+
+            // Add new created student object (& details) to the student list.
             studentList.Add(studentObject);
 
             Console.WriteLine("Add another? y/n: ");
@@ -52,10 +66,12 @@ namespace schoolTracker
           } // end while loop.
 
           Console.WriteLine();
-          Console.WriteLine("Capacity studentNames: {0}", studentNames.Capacity);
-          Console.WriteLine("Total studentNames: {0}", studentNames.Count);
-          Console.WriteLine("Capacity studentGrades: {0}", studentGrades.Capacity);
-          Console.WriteLine("Total studentNames: {0}", studentGrades.Count);
+          // Console.WriteLine("Capacity studentNames: {0}", studentNames.Capacity);
+          // Console.WriteLine("Total studentNames: {0}", studentNames.Count);
+          // Console.WriteLine("Capacity studentGrades: {0}", studentGrades.Capacity);
+          // Console.WriteLine("Total studentNames: {0}", studentGrades.Count);
+          Console.WriteLine("Capacity studentList: {0}", studentList.Capacity);
+          Console.WriteLine("Total studentList: {0}", studentList.Count);
           Console.WriteLine();
 
           // Uncomment this for test.
@@ -69,12 +85,19 @@ namespace schoolTracker
           // }
 
           // Display output of student details.
-          // for (int i=0; i<studentList.Count; i++) {
-          //   Console.WriteLine("Name: {0}, Grade: {1}", studentList[i].studentObject.Name, studentList[i].studentObject.Grade);
-          // }
-          foreach (var student in studentList) {
-            Console.WriteLine("Name: {0}, Grade: {1}", student.Name, student.Grade);
+          for (int i=0; i<studentList.Count; i++) {
+            Console.WriteLine("List => {0}, Name: {1}, Grade: {2}, Birthday: {2}, Address: {3}, Phone: {4}", studentList[i],
+                                                                                                             studentList[i].Name,
+                                                                                                             studentList[i].Grade,
+                                                                                                             studentList[i].Birthday,
+                                                                                                             studentList[i].Address,
+                                                                                                             studentList[i].Phone);
           }
+
+          // Uncomment to use foreach.
+          // foreach (var student in studentList) {
+          //   Console.WriteLine("Name: {0}, Grade: {1}", student.Name, student.Grade);
+          // }
         }
     }
 
