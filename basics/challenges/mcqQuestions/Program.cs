dd5fcbb4c960ee30cb1fe6c06e47f6f5b8c7e9f1
Hafidz <halfpheez92@gmail.com>
2018-11-14 18:36:01 +0800

    modified code, implemented object oriented for survey mcq challenge.

--- basics/challenges/mcqQuestions/Program.cs
+++ basics/challenges/mcqQuestions/Program.cs
@@ -1,40 +1,75 @@
 ﻿using System;
 
-namespace mcqQuestions
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("\nWhats your name?");
-            // var userName = Console.ReadLine();
-            var userName = GetInputAnswer();
+namespace mcqQuestions {
 
-            Console.WriteLine("\nHow old are you?");
-            // var userAge =  Console.ReadLine();
-            var userAge = GetInputAnswer();
+    // Implement Object Oriented.
+    class DataHandler {
 
-            Console.WriteLine("\nWhat month were your born in?");
-            // var userMonthDOB = Console.ReadLine();
-            var userMonthDOB = GetInputAnswer();
+      // Initialise fields.
+      public string userName;
+      public int userAge;
+      public string userMonthDOB;
 
-            Console.WriteLine("\nName: " + userName);
-            Console.WriteLine("Age: " + userAge);
-            Console.WriteLine("Month DOB: " + userMonthDOB);
+      // Add function to output all field values.
+      public void DisplayOutput() {
+        Console.WriteLine("\nName: " + userName);
+        Console.WriteLine("Age: " + userAge);
+        Console.WriteLine("Month DOB: " + userMonthDOB);
 
-            if (userMonthDOB == "march" || userMonthDOB == "March") {
-              Console.WriteLine("Zodiac Sign: Aries");
-            }
-            else if (userMonthDOB == "april" || userMonthDOB == "April") {
-              Console.WriteLine("Zodiac Sign: Taurus");
-            }
-            else if (userMonthDOB == "may" || userMonthDOB == "May") {
-              Console.WriteLine("Zodiac Sign: Gemini");
-            }
+        if (userMonthDOB == "march" || userMonthDOB == "March") {
+          Console.WriteLine("Zodiac Sign: Aries");
+        }
+        else if (userMonthDOB == "april" || userMonthDOB == "April") {
+          Console.WriteLine("Zodiac Sign: Taurus");
+        }
+        else if (userMonthDOB == "may" || userMonthDOB == "May") {
+          Console.WriteLine("Zodiac Sign: Gemini");
+        }
+      }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+          // Instantiate a data object.
+          var dataObject = new DataHandler();
+
+          Console.WriteLine("\nWhats your name?");
+          // var userName = Console.ReadLine();
+          // var userName = GetInputAnswer();
+          dataObject.userName = GetInputAnswer();
+
+          Console.WriteLine("\nHow old are you?");
+          // var userAge =  Console.ReadLine();
+          // var userAge = GetInputAnswer();
+          dataObject.userAge = int.Parse(GetInputAnswer());
+
+          Console.WriteLine("\nWhat month were your born in?");
+          // var userMonthDOB = Console.ReadLine();
+          // var userMonthDOB = GetInputAnswer();
+          dataObject.userMonthDOB = GetInputAnswer();
+
+          // Console.WriteLine("\nName: " + userName);
+          // Console.WriteLine("Age: " + userAge);
+          // Console.WriteLine("Month DOB: " + userMonthDOB);
+
+          // Invoke DisplayOutput()
+          dataObject.DisplayOutput();
+
+          // if (userMonthDOB == "march" || userMonthDOB == "March") {
+          //   Console.WriteLine("Zodiac Sign: Aries");
+          // }
+          // else if (userMonthDOB == "april" || userMonthDOB == "April") {
+          //   Console.WriteLine("Zodiac Sign: Taurus");
+          // }
+          // else if (userMonthDOB == "may" || userMonthDOB == "May") {
+          //   Console.WriteLine("Zodiac Sign: Gemini");
+          // }
         }
 
         static string GetInputAnswer() {
 
+          // Get input answer.
           var answer = Console.ReadLine();
 
           // Check if the param is empty, get new input and return new value.
