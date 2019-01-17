cde1128ee89864834562eac32fe8d35991e85fb0
Hafidz <halfpheez92@gmail.com>
2019-01-17 17:20:27 +0800

    added data annotations attributes for Concert Model class validation.

--- aspnet-mvc5-training/MVCRockers/MVCRockers/Models/Concert.cs
+++ aspnet-mvc5-training/MVCRockers/MVCRockers/Models/Concert.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +10,24 @@ namespace MVCRockers.Models
     public class Concert
     {
         // Add properties for Concert model class.
+        [Required(ErrorMessage = "A Name field is required!")]
+        [StringLength(20)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "A City field is required!")]
         public string City { get; set; }
+
+        [Display(Name = "Concert Date")]
         public DateTime ConcertDate { get; set; }
+
+        [Display(Name = "Backstage Passes")]
         public int Tickets { get; set; }
+
+        [DataType(DataType.Currency)]
         public double Price { get; set; }
+
+        [Required(ErrorMessage = "A Password field is required!")]
+        [RegularExpression(@"\d{3,9}\w{3,9}")]
+        public string Password { get; set; }
     }
 }
