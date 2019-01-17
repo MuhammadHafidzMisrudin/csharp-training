b4ba0ed3148d58d4066160b4cbf0c7b0b766f216
Hafidz <halfpheez92@gmail.com>
2019-01-17 12:55:20 +0800

    added Concert model and implemented Concert Controller.

---
+++ aspnet-mvc5-training/MVCRockers/MVCRockers/Controllers/ConcertController.cs
@@ -0,0 +1,27 @@
+﻿using MVCRockers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCRockers.Controllers
+{
+    public class ConcertController : Controller
+    {
+        // Add the object initializer with an instance and its properties.
+        Concert concert = new Concert() { Name = "TestData Concert", Price = 1.00, Tickets = 10, City = "New York", ConcertDate = DateTime.UtcNow };
+
+        // GET: Concert
+        public ActionResult Index()
+        {
+            return View(concert);
+        }
+
+        // Add Edit method to data.
+        public ActionResult Edit()
+        {
+            return View(concert);
+        }
+    }
+}
---
+++ aspnet-mvc5-training/MVCRockers/MVCRockers/Models/Concert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCRockers.Models
+{
+    public class Concert
+    {
+        // Add properties for Concert model class.
+        public string Name { get; set; }
+        public string City { get; set; }
+        public DateTime ConcertDate { get; set; }
+        public int Tickets { get; set; }
+        public double Price { get; set; }
+    }
+}
