7bca22d216d7c6439b5ec5fa62d2054bdb84543c
Hafidz <halfpheez92@gmail.com>
2019-01-24 12:38:36 +0800

    added events and implemented Stats class.

--- basics/challenges/mcqQuestions/Program.cs
+++ basics/challenges/mcqQuestions/Program.cs
@@ -1,83 +1,112 @@
 ﻿using System;
 
-namespace mcqQuestions {
+namespace mcqQuestions
+{
 
     // Implement Object Oriented.
-    class DataHandler {
+    class DataHandler
+    {
 
-      // Initialise fields.
-      public string userName;
-      public int userAge;
-      public string userMonthDOB;
+        // Initialise fields.
+        public string userName;
+        public int userAge;
+        public string userMonthDOB;
 
-      // Add function to output all field values.
-      public void DisplayOutput() {
-        Console.WriteLine("\nName: " + userName);
-        Console.WriteLine("Age: " + userAge);
-        Console.WriteLine("Month DOB: " + userMonthDOB);
+        // Add function to output all field values.
+        public void DisplayOutput()
+        {
+            Console.WriteLine("\nName: " + userName);
+            Console.WriteLine("Age: " + userAge);
+            Console.WriteLine("Month DOB: " + userMonthDOB);
 
-        if (userMonthDOB == "march" || userMonthDOB == "March") {
-          Console.WriteLine("Zodiac Sign: Aries");
+            if (userMonthDOB == "march" || userMonthDOB == "March")
+            {
+                Console.WriteLine("Zodiac Sign: Aries");
+            }
+            else if (userMonthDOB == "april" || userMonthDOB == "April")
+            {
+                Console.WriteLine("Zodiac Sign: Taurus");
+            }
+            else if (userMonthDOB == "may" || userMonthDOB == "May")
+            {
+                Console.WriteLine("Zodiac Sign: Gemini");
+            }
         }
-        else if (userMonthDOB == "april" || userMonthDOB == "April") {
-          Console.WriteLine("Zodiac Sign: Taurus");
-        }
-        else if (userMonthDOB == "may" || userMonthDOB == "May") {
-          Console.WriteLine("Zodiac Sign: Gemini");
-        }
-      }
     }
 
-    class Program {
-        static void Main(string[] args) {
+    class Program
+    {
 
-          // Instantiate a data object.
-          var dataObject = new DataHandler();
+        // Declare the event.
+        static public event Action Posted;
 
-          Console.WriteLine("\nWhats your name?");
-          // var userName = Console.ReadLine();
-          // var userName = GetInputAnswer();
-          dataObject.userName = GetInputAnswer();
+        static void Main(string[] args)
+        {
 
-          Console.WriteLine("\nHow old are you?");
-          // var userAge =  Console.ReadLine();
-          // var userAge = GetInputAnswer();
-          dataObject.userAge = int.Parse(GetInputAnswer());
+            // Create a Stats system.
+            // Instantiate a stats class.
+            var stats = new Stats();
+            stats.Start();
 
-          Console.WriteLine("\nWhat month were your born in?");
-          // var userMonthDOB = Console.ReadLine();
-          // var userMonthDOB = GetInputAnswer();
-          dataObject.userMonthDOB = GetInputAnswer();
+            // Instantiate a data object class.
+            var dataObject = new DataHandler();
 
-          // Console.WriteLine("\nName: " + userName);
-          // Console.WriteLine("Age: " + userAge);
-          // Console.WriteLine("Month DOB: " + userMonthDOB);
+            Console.WriteLine("\nWhats your name?");
+            // var userName = Console.ReadLine();
+            // var userName = GetInputAnswer();
+            dataObject.userName = GetInputAnswer();
 
-          // Invoke DisplayOutput()
-          dataObject.DisplayOutput();
+            Console.WriteLine("\nHow old are you?");
+            // var userAge =  Console.ReadLine();
+            // var userAge = GetInputAnswer();
+            dataObject.userAge = int.Parse(GetInputAnswer());
 
-          // if (userMonthDOB == "march" || userMonthDOB == "March") {
-          //   Console.WriteLine("Zodiac Sign: Aries");
-          // }
-          // else if (userMonthDOB == "april" || userMonthDOB == "April") {
-          //   Console.WriteLine("Zodiac Sign: Taurus");
-          // }
-          // else if (userMonthDOB == "may" || userMonthDOB == "May") {
-          //   Console.WriteLine("Zodiac Sign: Gemini");
-          // }
+            Console.WriteLine("\nWhat month were your born in?");
+            // var userMonthDOB = Console.ReadLine();
+            // var userMonthDOB = GetInputAnswer();
+            dataObject.userMonthDOB = GetInputAnswer();
+
+            // Console.WriteLine("\nName: " + userName);
+            // Console.WriteLine("Age: " + userAge);
+            // Console.WriteLine("Month DOB: " + userMonthDOB);
+
+            // Check if nothing has registered to the posted event, if the event is null.
+            // Invoke the event.
+            if (Posted != null)
+            {
+                Posted();
+            }
+            
+
+            // Invoke DisplayOutput().
+            dataObject.DisplayOutput();
+
+            // if (userMonthDOB == "march" || userMonthDOB == "March") {
+            //   Console.WriteLine("Zodiac Sign: Aries");
+            // }
+            // else if (userMonthDOB == "april" || userMonthDOB == "April") {
+            //   Console.WriteLine("Zodiac Sign: Taurus");
+            // }
+            // else if (userMonthDOB == "may" || userMonthDOB == "May") {
+            //   Console.WriteLine("Zodiac Sign: Gemini");
+            // }
+
+            Console.ReadKey();
         }
 
-        static string GetInputAnswer() {
+        static string GetInputAnswer()
+        {
 
-          // Get input answer.
-          var answer = Console.ReadLine();
+            // Get input answer.
+            var answer = Console.ReadLine();
 
-          // Check if the param is empty, get new input and return new value.
-          if (answer == "") {
-            Console.WriteLine("Error! Valid input required. Try again: ");
-            return Console.ReadLine();
-          }
-          return answer;
+            // Check if the param is empty, get new input and return new value.
+            if (answer == "")
+            {
+                Console.WriteLine("Error! Valid input required. Try again: ");
+                return Console.ReadLine();
+            }
+            return answer;
         }
     }
 }
---
+++ basics/challenges/mcqQuestions/Stats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mcqQuestions
+{
+    class Stats
+    {
+        public void Start()
+        {
+            Program.Posted += HasPosted;
+        }
+
+        void HasPosted()
+        {
+            Console.WriteLine("\nSurvey posted, run stats.");
+        }
+    }
+}
