b27998b853796fe83aecfb86fe1d65cfac4f2f1c
Hafidz <halfpheez92@gmail.com>
2019-02-15 10:33:52 +0800

    removed project.

--- aspnet-mvc5-training/ReactAspx/ReactAspx/Content/src/food.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const ReactDOM = require("react-dom");
-ReactDOM.render(React.createElement("div", null,
-    React.createElement("h2", null, "Test123456")), document.getElementById("foodorder"));
-//# sourceMappingURL=food.js.map
--- aspnet-mvc5-training/ReactAspx/ReactAspx/Content/src/food.tsx
+++
@@ -1,9 +0,0 @@
-﻿import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-ReactDOM.render(
-    <div>
-        <h2>Test123456</h2>
-    </div>,
-    document.getElementById("foodorder")
-);
